eecf32f5f879319ff66ce7a7da77c69128a6f20a
Willy <willy810308@gmail.com>
2022-06-06 22:18:54 +0800

    1. 另立專案

--- Form1.cs
+++ Form1.cs
@@ -10,16 +10,20 @@ using System.Windows.Forms;
 using System.Net;
 using HtmlAgilityPack;
 using System.IO;
+using MyLottery.Ticket;
+using MyLottery.Computer;
 
 namespace LottoCalculator
 {
     public partial class Form1 : Form
     {
-        LottoCal lc;
+        //LottoCal lc;
+        LottoComputer lc;
         public Form1()
         {
             InitializeComponent();
-            lc = new LottoCal();
+            //lc = new LottoCal();
+            lc = new LottoComputer();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -40,16 +44,24 @@ namespace LottoCalculator
                 var str = sr.ReadLine();
                 var infos = str.Split(',');
 
-                LottoData ld = new LottoData();
-                ld.Index = infos[0];
-                ld.Date = infos[1];
-                ld.Star1 = Convert.ToByte(infos[2]);
-                ld.Star2 = Convert.ToByte(infos[3]);
-                ld.Star3 = Convert.ToByte(infos[4]);
-                ld.Star4 = Convert.ToByte(infos[5]);
-                ld.Star5 = Convert.ToByte(infos[6]);
-                ld.Star6 = Convert.ToByte(infos[7]);
-                ld.Special = Convert.ToByte(infos[8]);
+                //LottoData ld = new LottoData();
+                //ld.Index = infos[0];
+                //ld.Date = infos[1];
+                //ld.Star1 = Convert.ToByte(infos[2]);
+                //ld.Star2 = Convert.ToByte(infos[3]);
+                //ld.Star3 = Convert.ToByte(infos[4]);
+                //ld.Star4 = Convert.ToByte(infos[5]);
+                //ld.Star5 = Convert.ToByte(infos[6]);
+                //ld.Star6 = Convert.ToByte(infos[7]);
+                //ld.Special = Convert.ToByte(infos[8]);
+                List<byte> stars = new List<byte>();
+                stars.Add(Convert.ToByte(infos[2]));
+                stars.Add(Convert.ToByte(infos[3]));
+                stars.Add(Convert.ToByte(infos[4]));
+                stars.Add(Convert.ToByte(infos[5]));
+                stars.Add(Convert.ToByte(infos[6]));
+                stars.Add(Convert.ToByte(infos[7]));
+                LottoResultInfo ld = new LottoResultInfo(stars, Convert.ToByte(infos[8]), infos[1], infos[0]);
 
                 lc.AddRecordData(ld);
                 cnt++;
@@ -61,7 +73,14 @@ namespace LottoCalculator
 
         private void button3_Click(object sender, EventArgs e)
         {
-            button3.Text = lc.Contain(5, 15, 26, 27, 34, 44).ToString();            
+            List<byte> stars = new List<byte>();
+            stars.Add(2);
+            stars.Add(14);
+            stars.Add(16);
+            stars.Add(22);
+            stars.Add(46);
+            stars.Add(37);
+            button3.Text = lc.Contain(stars).ToString();            
         }
 
         private void button4_Click(object sender, EventArgs e)
