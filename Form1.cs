5d1d725fb0334003cc308eb48e120889a35142a1
Willy <willy810308@gmail.com>
2022-06-09 23:19:43 +0800

    1. 移除LottoCal.cs

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Net;
 using HtmlAgilityPack;
 using System.IO;
+using MyLottery;
 using MyLottery.Ticket;
 using MyLottery.Computer;
 
@@ -17,58 +18,34 @@ namespace LottoCalculator
 {
     public partial class Form1 : Form
     {
-        //LottoCal lc;
         LottoComputer lc;
         public Form1()
         {
             InitializeComponent();
-            //lc = new LottoCal();
             lc = new LottoComputer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //button1.Text = lc.WinningRate(6, 0);
-
             var one = lc.DrawWithoutRecord();
             button1.Text = one.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int cnt = 0;
-
-            StreamReader sr = new StreamReader(@"E:\user\Desktop\樂透歷年開獎.csv");
-            while(!sr.EndOfStream)
-            {
-                var str = sr.ReadLine();
-                var infos = str.Split(',');
-
-                //LottoData ld = new LottoData();
-                //ld.Index = infos[0];
-                //ld.Date = infos[1];
-                //ld.Star1 = Convert.ToByte(infos[2]);
-                //ld.Star2 = Convert.ToByte(infos[3]);
-                //ld.Star3 = Convert.ToByte(infos[4]);
-                //ld.Star4 = Convert.ToByte(infos[5]);
-                //ld.Star5 = Convert.ToByte(infos[6]);
-                //ld.Star6 = Convert.ToByte(infos[7]);
-                //ld.Special = Convert.ToByte(infos[8]);
-                List<byte> stars = new List<byte>();
-                stars.Add(Convert.ToByte(infos[2]));
-                stars.Add(Convert.ToByte(infos[3]));
-                stars.Add(Convert.ToByte(infos[4]));
-                stars.Add(Convert.ToByte(infos[5]));
-                stars.Add(Convert.ToByte(infos[6]));
-                stars.Add(Convert.ToByte(infos[7]));
-                LottoResultInfo ld = new LottoResultInfo(stars, Convert.ToByte(infos[8]), infos[1], infos[0]);
-
-                lc.AddRecordData(ld);
-                cnt++;
-            }
-            sr.Close();
-
-            button2.Text = cnt.ToString();
+            List<CsvItem> csvItems = new List<CsvItem>();
+            csvItems.Add(CsvItem.Index);
+            csvItems.Add(CsvItem.Date);
+            csvItems.Add(CsvItem.Star1);
+            csvItems.Add(CsvItem.Star2);
+            csvItems.Add(CsvItem.Star3);
+            csvItems.Add(CsvItem.Star4);
+            csvItems.Add(CsvItem.Star5);
+            csvItems.Add(CsvItem.Star6);
+            csvItems.Add(CsvItem.Star7);
+            lc.LoadHistoryCSV(@"E:\user\Desktop\樂透歷年開獎.csv", csvItems);
+            
+            button2.Text = lc.NumberOfHistory.ToString();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -96,6 +73,34 @@ namespace LottoCalculator
             while (!ok);
 
             button4.Text = cnt.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Text = LottoComputer.WinningCount(1, 0).ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Text = LottoComputer.WinningRate().ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button7.Text = LottoComputer.Expectation(0).ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            LottoTicket lottoTicket = new LottoTicket(true);
+            var Awards = lc.MatchHistory(lottoTicket).Where(r => r != MyLottery.Award.None);
+            button8.Text = Awards.Count().ToString();
+
+            Console.Clear();
+            foreach(var award in Awards)
+            {
+                Console.WriteLine(award);
+            }
         }
     }
 }
